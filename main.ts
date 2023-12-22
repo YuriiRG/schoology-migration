87ee4902e8a36b3674928e67bccc2b3d8c9505ad
YuriiRG <yuriirg@tutanota.com>
2023-12-22 18:15:53 +0100

    Support arbitrary nesting, few fixes

--- main.ts
+++ main.ts
@@ -1,62 +1,64 @@
 import { XMLParser } from "fast-xml-parser";
-import { createWriteStream, existsSync, readFileSync } from "fs";
+import { createWriteStream, existsSync, readFileSync, readdirSync } from "fs";
 import { parse as parseHtml } from "node-html-parser";
 type Lesson = {
   title: string | number;
   "@_identifierref"?: string;
 };
 
+type Block =
+  | {
+      title: string;
+      item: Block[];
+    }
+  | Lesson;
+
 const xmlParser = new XMLParser({ ignoreAttributes: false });
-const manifest: {
-  // block
-  title: string;
-  item: (
-    | {
-        // group / lesson
-        title: string;
-        item: Lesson[];
-      }
-    | Lesson
-  )[];
-}[] = xmlParser.parse(
+const manifest: Block[] = xmlParser.parse(
   readFileSync("./data/imsmanifest.xml", { encoding: "utf-8" })
 ).manifest.organizations.organization.item.item;
 
 const printer = createWriteStream("results.txt");
 
-for (const block of manifest) {
-  printer.write(`${block.title}\n`);
-  for (const group of block.item) {
-    if ("item" in group) {
-      printer.write(`  ${group.title}\n`);
-      for (const lesson of group.item) {
-        if ("item" in lesson) {
-          // skip very nested groups because they are not records
-          continue;
-        }
-        processLesson(lesson);
-      }
+processBlocks(manifest);
+
+function processBlocks(blocks: Block[]) {
+  for (const block of blocks) {
+    if ("item" in block) {
+      printer.write(`Block: ${block.title}\n`);
+      processBlocks(block.item);
     } else {
-      processLesson(group);
+      processLesson(block);
     }
   }
 }
 
 function processLesson(lesson: Lesson) {
+  if (typeof lesson.title === "number") {
+    lesson.title = lesson.title.toFixed(2).padStart(5, "0");
+  }
   //printer.write(`${lesson.title}: ${lesson["@_identifierref"]}`);
   if (
     lesson["@_identifierref"] !== undefined &&
-    existsSync(
-      `./data/${lesson["@_identifierref"]}/${lesson["@_identifierref"]}.html`
-    )
+    readdirSync(`./data/${lesson["@_identifierref"]}`).length > 0
   ) {
+    if (
+      !existsSync(
+        `./data/${lesson["@_identifierref"]}/${lesson["@_identifierref"]}.html`
+      )
+    ) {
+      console.log(
+        `No html in "${lesson["title"]}" ${lesson["@_identifierref"]}`
+      );
+      return;
+    }
     const html = parseHtml(
       readFileSync(
         `./data/${lesson["@_identifierref"]}/${lesson["@_identifierref"]}.html`,
         { encoding: "utf-8" }
       )
     );
-    const links = html
+    let links = html
       .querySelectorAll("iframe")
       .map((elem) => elem.attributes.src)
       .concat(html.querySelectorAll("a").map((elem) => elem.attributes.href))
@@ -66,6 +68,7 @@ function processLesson(lesson: Lesson) {
           ? `https://youtu.be/${url.split("/").at(-1)!}`
           : url
       );
+    links = links.filter((item, pos) => links.indexOf(item) == pos);
     printer.write(`${lesson.title}\t${links.join(" ")}\n`);
   } else {
     printer.write(`${lesson.title}\n`);
